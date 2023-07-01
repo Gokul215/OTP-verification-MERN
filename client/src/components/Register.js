26d7adf885fcbbf990c6675b931da2aa3c64a81f
Gokul215 <gokulbabu811@gmail.com>
2023-07-01 19:28:38 +0530

    profile changed in register component

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import avatar from "../assets/profile.png";
+
 import {Link} from "react-router-dom"
 import {useFormik} from 'formik';
 import {Toaster} from 'react-hot-toast';
-import {validatepassword} from "../helper/validate";
-import convert from "../helper/convert";
+import {registerValidation} from "../helper/validate";
+import { useRef } from "react";
+// import convert from "../helper/convert";
 
 
 
@@ -13,18 +15,21 @@ import convert from "../helper/convert";
 export default function Register() {
 
   const [file,setfile]=useState();
+  const image=useRef("");
  
   const formik=useFormik({
        initialValues:
        {
-         password:''
+        email: '',
+        username: '',
+        password : ''
        }, 
        validateOnBlur:false,
        validateOnChange:false,
-       validate:validatepassword,
+       validate:registerValidation,
 
        onSubmit:async values=>{
-        values = await Object.assign(values, { profile : file || ''})
+        values = await Object.assign(values, { profile : file })
         console.log(values)
        }
       
@@ -33,9 +38,25 @@ export default function Register() {
 
 
   const  onUpload = async e =>{
-    const base64 = convert(e.target.files[0]);
-    setfile(base64);
+    // const base64 = convert(e.target.files[0]);
+    // setfile(base64);
+    const file = e.target.files[0];
 
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onloadend = () => {
+        setfile(reader.result);
+      };
+
+     
+    }
+
+  }
+
+  const uploadphoto =()=>{
+    image.current.click();
   }
 
   return (
@@ -51,37 +72,44 @@ export default function Register() {
       </div>
 
      
-      <form onSubmit={formik.handleSubmit}>
+      <form name="form" onSubmit={formik.handleSubmit}>
 
       <div className="row justify-content-center ">
       <div class="col-md-2-sm-1  px-0"  >
 
       <label htmlFor="proile">
-        <img style={{height:200}}
-          className="img-responsive "
-          src={file || avatar}
-          alt="proile"
-        /></label>
 
-        <input  onChange={onUpload} type="file" id="profile" name="profile" />
+      
+        <img style={{height:200}}
+          className="img-responive "
+          src={file || avatar}
+           onClick={uploadphoto}
+          
+          alt="proile"
+        
+        />
+      
+      </label>
+
+        <input  values= {formik.values.profile} style={{display:"none"}} ref={image}  onChange={onUpload} type="file" id="profile" name="profile" />
         </div>
     
       </div>
       <div className="row justify-content-center " onSubmit={formik.handleSubmit}>
           <input className="text  " style={{marginTop:10}} type="email" name="email" onChange={formik.handleChange} value={formik.values.email} placeholder="email" ></input>
         
-
+          
         </div>
         <div className="row justify-content-center " onSubmit={formik.handleSubmit}>
         
-          <input className="text " style={{marginTop:10}} type="Username" name="Username" onChange={formik.handleChange} value={formik.values.Username} placeholder="username" ></input>
-        
+          <input className="text " style={{marginTop:10}} type="text" name="username" onChange={formik.handleChange} value={formik.values.username} placeholder="username" ></input>
+         
 
         </div>
         <div className="row justify-content-center " onSubmit={formik.handleSubmit}>
           
           <input className="text  " style={{marginTop:10}} type="password" name="password" onChange={formik.handleChange} value={formik.values.password} placeholder="password" ></input>
-
+         
         </div>
         <div  className="row justify-content-center  ">
           <button  type='submit' style={{marginTop:10}} onSubmit={formik.handleSubmit} className="btn btn-success">Sign in</button>
--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -57,4 +57,32 @@ export async function resetpassword(values){
   return errors;
 }
 
+/* verify register*/
+
+export async function registerValidation(values){
+  const errors = verifyusername({}, values);
+  verifypassword(errors, values);
+  emailVerify(errors, values);
+
+  return errors;
+}
+
+/** validate profile page */
+export async function profileValidation(values){
+  const errors = emailVerify({}, values);
+  return errors;
+}
+
+/** validate email */
+function emailVerify(error ={}, values){
+  if(!values.email){
+      error.email = toast.error("Email Required...!");
+  }else if(values.email.includes(" ")){
+      error.email = toast.error("Wrong Email...!")
+  }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+      error.email = toast.error("Invalid email address...!")
+  }
+
+  return error;
+}
 
