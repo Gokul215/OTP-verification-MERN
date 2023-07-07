bdedb91c29aa86b36304975a81cdc76491fe8b5a
Gokul215 <gokulbabu811@gmail.com>
2023-07-07 19:20:00 +0530

    helper getusername created

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -182,8 +182,8 @@ body: {
 export async function updateUser(req,res){
     try {
         
-        const id = req.query.id;
-        // const { userId } = req.user;
+        //const id = req.query.id;
+        const { userId } = req.user;
 
         if(userId){
             const body = req.body;
