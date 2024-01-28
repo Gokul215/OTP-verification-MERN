9dcc2da12ca6f318f336126ee68e81d3c7e48ca0
Gokul215 <gokulbabu811@gmail.com>
2024-01-28 12:19:54 +0530

    finished

---
+++ server/config.js
@@ -0,0 +1,10 @@
+export default{
+    JWT_SECRET:"JidYUExyO7w2zaCPgN2G9Ua18Dco0nH58IgvnZCLaMc=",
+    EMAIL:"gokula3303@gmail.com",
+    
+    PASSWORD:"opwgyttkpzfwupwh",
+   
+    URI:"mongodb+srv://gokulbabu811:4321@cluster0.imznwfk.mongodb.net/db?retryWrites=true&w=majority"
+}
+// EMAIL:"gokulbabu811@gmail.com",
+ // PASSWORD:"tkjwftzrvnepjlbg",
